130fe0e8decb7be27051482090f0efaeb181781f
bryankennethblando <bryankennethblando@gmail.com>
2026-05-22 20:36:27 +0800

    final track-bit-top5

--- app.py
+++ app.py
@@ -1,14 +1,51 @@
 import streamlit as st
 import pandas as pd
 
+from src.api_fecher import update_data
+
+st.set_page_config(page_title="Crypto Tracker", page_icon="📈", layout="wide")
+
 st.title("Crypto Price Tracker MVP")
-st.write("Tracking the top 10 highest-priced cryptocurrencies.")
+st.write("Tracking the top 5 highest-priced cryptocurrencies.")
 
-df = pd.read_csv("data/crypto_data.csv")
+if st.button("Fetch Live Prices 🔄"):
+    with st.spinner("Fetching data from Binance"):
+        success = update_data()
 
-st.write("### Raw Data")
-st.dataframe(df)
+        if success:
+            st.success("Data updated successfully!")
+        else:
+            st.error("Failed to update data.")
 
-st.write("### Price Comparison")
-chart_data = df.set_index('symbol')['price']
-st.bar_chart(chart_data)
+try:
+    df = pd.read_csv("data/crypto_data.csv")
+
+    # --- NEW: Dashboard KPI Metrics ---
+    st.write("### 🏆 Top 5 Market Leaders")
+    # Create 3 columns
+    col1, col2, col3, col4, col5 = st.columns(5)
+    
+    # Display the top 3 coins in the columns using st.metric
+    with col1:
+        st.metric(label=df.iloc[0]['symbol'], value=f"${df.iloc[0]['price']:,.2f}")
+    with col2:
+        st.metric(label=df.iloc[1]['symbol'], value=f"${df.iloc[1]['price']:,.2f}")
+    with col3:
+        st.metric(label=df.iloc[2]['symbol'], value=f"${df.iloc[2]['price']:,.2f}")
+    with col4:
+        st.metric(label=df.iloc[3]['symbol'], value=f"${df.iloc[3]['price']:,.2f}")
+    with col5:
+        st.metric(label=df.iloc[4]['symbol'], value=f"${df.iloc[4]['price']:,.2f}")
+    
+    st.markdown("---") # Visual divider
+    # ----------------------------------
+
+    st.write("### Price Comparison")
+    chart_data = df.set_index('symbol')['price']
+    st.bar_chart(chart_data)
+
+    st.write("### Raw Data Table")
+    st.dataframe(df, use_container_width=True)
+
+except FileNotFoundError:
+    st.warning("No data found. Please click the  'Fetch Live Prices' button above.")
--- src/api_fecher.py
+++ src/api_fecher.py
@@ -1,20 +1,25 @@
 import requests
 import pandas as pd
 
-# setting up the url connection
-url = url = "https://api.binance.com/api/v3/ticker/price"
-response = requests.get(url)
+def update_data():
 
-# fetching the data in json format
-data = response.json()
+    print("Fetching fresh data from API....")
+    url = "https://api.binance.com/api/v3/ticker/price"
+    response = requests.get(url)
 
-# insetring the json file into dataframe
-df = pd.DataFrame(data)
+    if response.status_code == 200:
+        data = response.json()
 
-# to get the values of the top10 coins
-df['price'] = pd.to_numeric(df['price'])
-top_10_df= df.sort_values(by='price', ascending=False).head(10)
+        df = pd.DataFrame(data)
 
-# to saved the dataframe into a csv file
-top_10_df.to_csv("data/crypto_data.csv", index=False)
-print("Saved Binance data to CSV!")
+        df['price'] = pd.to_numeric(df['price'])
+        top_10_df= df.sort_values(by='price', ascending=False).head(10)
+
+        top_10_df.to_csv("data/crypto_data.csv", index=False)
+        print("Successfully updated crypto_data.csv!")
+        return True
+    else:
+        print("Failed to Fetch data")
+        return
+
+
